d21a3a749bb36b100d33882411b13b092c33935d
Anshu Kushwaha <anshukushwaha112@gmail.com>
2024-12-01 16:13:50 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+// import java.awt.Color;
+// import javax.swing.ImageIcon;
+// import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // JFrame = a GUI window to add components
+        
+        /* 
+        JFrame frame = new JFrame();  //creates JFrame
+        frame.setTitle("JFrame title"); // sets title frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of application
+        frame.setResizable(false); //prevent frame from resize
+        frame.setSize(480,480); //sets the x and y dimension
+        frame.setVisible(true); //make frame visible
+
+        ImageIcon image = new ImageIcon("channels4_profile.jpg"); //create an ImageIcon
+        frame.setIconImage(image.getImage()); //change icon frame
+        frame.getContentPane().setBackground(Color.BLACK); //Change colour of background
+        */
+
+
+        new MyFrame(); //we can also use this when we have no gonna modify
+        //MyFrame myFrame = new MyFrame(); // use this to modify frame
+        
+    }
+}
